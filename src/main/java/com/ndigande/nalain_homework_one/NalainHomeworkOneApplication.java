5a02f553758228cef15ea4bc0d70866f8c4ac25c
Nalain <possenalain21@gmail.com>
2022-06-25 21:24:43 +0300

    initial commit homework without dependencies

---
+++ src/main/java/com/ndigande/nalain_homework_one/NalainHomeworkOneApplication.java
@@ -0,0 +1,13 @@
+package com.ndigande.nalain_homework_one;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NalainHomeworkOneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NalainHomeworkOneApplication.class, args);
+	}
+
+}
