37bb80ec2af092199bcb78607f305e13a020c91d
Nalain <possenalain21@gmail.com>
2022-06-26 08:41:37 +0300

    the user entity wouldnt be created, naming issues I changed it to usr

--- src/main/java/com/ndigande/nalain_homework_one/product/entity/Product.java
+++ src/main/java/com/ndigande/nalain_homework_one/product/entity/Product.java
@@ -4,7 +4,6 @@ import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.*;
-import javax.validation.constraints.Email;
 import java.util.Date;
 
 @Entity
@@ -35,40 +34,4 @@ public class Product {
     @Temporal(TemporalType.DATE)
     private Date expirationDate;
 
-    @Entity
-    @Table(name ="USER")
-    @Getter
-    @Setter
-    public static class User {
-        @Id
-        @GeneratedValue(generator = "User")
-        @SequenceGenerator(
-                name = "User",
-        sequenceName = "USER_ID_SEQ"
-        )
-        private Long id;
-
-        @Column(name = "NAME",
-                length = 50,
-                nullable = false)
-        private String name;
-
-        @Column(name = "SURNAME",
-                length = 50,
-                nullable = false)
-        private String surname;
-
-        @Email
-        @Column(name = "EMAIL",
-                length = 50,
-                nullable = false)
-        private String email;
-
-
-        @Column(name = "TELEPHONE",
-                length = 15,
-                nullable = false)
-        private String phoneNumber;
-
-    }
 }
